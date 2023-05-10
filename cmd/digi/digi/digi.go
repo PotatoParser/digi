6aef491175060b71b77ad60f0aa8a8076ea84816
Wilson Nguyen <45542237+PotatoParser@users.noreply.github.com>
2023-05-10 09:37:17 +0000

    Add sidecar

--- cmd/digi/digi/digi.go
+++ cmd/digi/digi/digi.go
@@ -190,6 +190,7 @@ var buildCmd = &cobra.Command{
 					imagePaths := []string{
 						filepath.Join(workDir, imageFile, "Dockerfile"),
 						filepath.Join(workDir, imageFile),
+						imageFile,
 					}
 
 					pathFound := false
@@ -624,6 +625,7 @@ var runCmd = &cobra.Command{
 		visual, _ := cmd.Flags().GetBool("enable-visual")
 		deployFile, _ := cmd.Flags().GetString("deploy-file")
 		persistentVolume, _ := cmd.Flags().GetBool("persistent-volume")
+		sidecars, _ := cmd.Flags().GetStringSlice("sidecar")
 
 		kopfLog := "false"
 		if k, _ := cmd.Flags().GetBool("kopf-log"); k {
@@ -651,6 +653,10 @@ var runCmd = &cobra.Command{
 			persistentVolumeSize, _ := cmd.Flags().GetString("pv-size")
 			persistentVolumePath, _ := cmd.Flags().GetString("pv-path")
 			runFlag += fmt.Sprintf(" --set persistent_volume.path=%s,persistent_volume.size=%s", persistentVolumePath, persistentVolumeSize)
+		}
+
+		for _, sidecar := range sidecars {
+			runFlag += fmt.Sprintf(" --set sidecar.%s=1", sidecar)
 		}
 
 		var wg sync.WaitGroup
